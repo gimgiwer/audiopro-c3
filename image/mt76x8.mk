9258cb89124da7e271a04dc811410abdef0d57ec
gimgiwer <gimgiwer@gmail.com>
2026-08-16 13:35:32 +0300

    feat(security): enable HTTPS with 100-year auto-generated certificate, redirect toggle and cert download

--- image/mt76x8.mk
+++ image/mt76x8.mk
@@ -1070,7 +1070,8 @@ define Device/audiopro_c3
   KERNEL := kernel-bin | append-dtb | lzma | uImage lzma -O svr4
   DEVICE_PACKAGES := kmod-sound-mt7620 alsa-utils alsa-lib \
                      shairport-sync-mbedtls librespot squeezelite gmrender-resurrect snapclient mpg123 \
-                     avahi-nodbus-daemon zram-swap mcud
+                     avahi-nodbus-daemon zram-swap mcud \
+                     uhttpd uhttpd-mod-ubox px5g-mbedtls
   SUPPORTED_DEVICES += audiopro,c3 linkplay,a28
 endef
 TARGET_DEVICES += audiopro_c3
